7d4e57347974dce58d8f0835d92911b17e825444
MsUliana <ulianadzoba@gmail.com>
2020-02-12 23:08:53 +0200

    Updates

---
+++ precache-manifest.ba1043c1f88d6292de2132199d385a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb41efe169fd0ed49e4fce55cb091708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427e68970dd0ed17f7d2",
+    "url": "/static/css/main.99b3ee84.chunk.css"
+  },
+  {
+    "revision": "4b4e5a5d9ad60e4d55d6",
+    "url": "/static/js/2.02bd3e07.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.02bd3e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427e68970dd0ed17f7d2",
+    "url": "/static/js/main.377ba1aa.chunk.js"
+  },
+  {
+    "revision": "a68774fa7ae1fabcc525",
+    "url": "/static/js/runtime-main.781de3bf.js"
+  }
+]);
