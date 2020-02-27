7e555019af30d65386da75162ef33d02b0d77989
MsUliana <ulianadzoba@gmail.com>
2020-02-27 19:59:35 +0200

    add additional meta tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Dashboard from '../src/hoc/Dashboard/Dashboard';
 import Quiz from './components/Quiz/Quiz';
 import { Route, Switch } from 'react-router-dom';
-// import Auth from './components/Auth/Auth';
 import HomePage from './components/HomePage/HomePage';
 
 function App() {
@@ -12,9 +11,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/quiz" exact component={Quiz} />
-          {/* <Route path="/login" exact component={Auth} /> */}
         </Switch>
-
       </Dashboard>
     </div>
   );
